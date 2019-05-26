af41ad07d2e277f4de830b619949823755b2d6df
Shuzhao Li <shuzhao.li@gmail.com>
2019-05-26 10:11:35 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name='hiconet',
@@ -46,19 +46,19 @@ setup(
   python_requires='>=3',
 
   data_files=[
-    ('datasets/SDY80', ['datasets/SDY80/project.yaml', 
-                        'datasets/SDY80/BTMactivity_SDY80.txt',
-                        'datasets/SDY80/neut_ab_titer__data_matrix.txt',
-                        'datasets/SDY80/neut_ab_titer__observation_annotation.txt',
-                        'datasets/SDY80/SDY80_fcs__data_matrix.txt',
-                        'datasets/SDY80/SDY80_fcs__observation_annotation.txt',
-                        'datasets/SDY80/biosample.txt',
+    ('datasets/SDY80', ['hiconet/datasets/SDY80/project.yaml', 
+                        'hiconet/datasets/SDY80/BTMactivity_SDY80.txt',
+                        'hiconet/datasets/SDY80/neut_ab_titer__data_matrix.txt',
+                        'hiconet/datasets/SDY80/neut_ab_titer__observation_annotation.txt',
+                        'hiconet/datasets/SDY80/SDY80_fcs__data_matrix.txt',
+                        'hiconet/datasets/SDY80/SDY80_fcs__observation_annotation.txt',
+                        'hiconet/datasets/SDY80/biosample.txt',
                         ]),
-    ('datasets/Colombia', ['datasets/Colombia/project.yaml', 
-                        'datasets/Colombia/formatted_BTMs_diag_basel.txt',
-                        'datasets/Colombia/HILIC_pos_diag_basel.txt',
-                        'datasets/Colombia/HILIC_pos_diag_basel_feature_annotation.txt',
-                        'datasets/Colombia/samples_meta.txt',
+    ('datasets/Colombia', ['hiconet/datasets/Colombia/project.yaml', 
+                        'hiconet/datasets/Colombia/formatted_BTMs_diag_basel.txt',
+                        'hiconet/datasets/Colombia/HILIC_pos_diag_basel.txt',
+                        'hiconet/datasets/Colombia/HILIC_pos_diag_basel_feature_annotation.txt',
+                        'hiconet/datasets/Colombia/samples_meta.txt',
                         ])
   ],
 
